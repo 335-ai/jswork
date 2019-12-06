c94e1314cc3dc72adeafc559b75db709b19c7a08
335-ai <617537516@qq.com>
2019-12-06 12:19:41 +0800

    work14

--- work14/index.js
+++ work14/index.js
@@ -14,7 +14,7 @@ function seckill() {    //获取当前时间
         m = parseInt((remaining /60) % 60);
         s = parseInt(remaining % 60);
 
-        d = d < 10 ? '0' + d : d;
+        d = d < 10 ? '0' + d : d;  
         h = h < 10 ? '0' + h : h;
         m = m < 10 ? '0' + m : m;
         s = s < 10 ? '0' + s : s;
